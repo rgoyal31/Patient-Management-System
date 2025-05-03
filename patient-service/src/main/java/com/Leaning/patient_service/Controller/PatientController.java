6665dab947fc3a13ae55042d7cf1c526921d83b7
prgoyal1 <rakshit.goyal@amadeus.com>
2025-05-04 00:39:12 +0530

    Adding the OpenAPI, used by the swagger to generate to generate the documentation, json can be found on the address: localhost:port/v3/api-docs

--- patient-service/src/main/java/com/Leaning/patient_service/Controller/PatientController.java
+++ patient-service/src/main/java/com/Leaning/patient_service/Controller/PatientController.java
@@ -4,6 +4,8 @@ import com.Leaning.patient_service.DTO.PatientResponseDTO;
 import com.Leaning.patient_service.DTO.PatientResquestDTO;
 import com.Leaning.patient_service.DTO.validators.CreatePatientValidationGroup;
 import com.Leaning.patient_service.Service.PatientService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import jakarta.validation.groups.Default;
 import lombok.AllArgsConstructor;
@@ -18,28 +20,34 @@ import java.util.List;
 @AllArgsConstructor
 @RequestMapping("/api/patient")
 @Slf4j
+@Tag(name = "Patients", description = "Api for managing Patients")
 public class PatientController {
 
     private final PatientService patientService;
+
     @GetMapping(path = "/getAll")
+    @Operation(summary = "Get All Patients")
     public ResponseEntity<List<PatientResponseDTO>> getAllPatients(){
         log.info("Inside the getAllPatientMethods in the controller");
         return ResponseEntity.ok(patientService.getAllPatient());
     }
 
     @PostMapping(path = "/create")
+    @Operation(summary = "Create a New Patients")
     public ResponseEntity<PatientResponseDTO> createPatient(
             @Validated({Default.class, CreatePatientValidationGroup.class}) @RequestBody PatientResquestDTO patientResquestDTO){
         return ResponseEntity.ok(patientService.registerPatient(patientResquestDTO));
     }
 
-    @PostMapping(path = "/update/{email}")
+    @PutMapping(path = "/update/{email}")
+    @Operation(summary = "Update the Patients")
     public ResponseEntity<PatientResponseDTO> updatePatient(@PathVariable String email,
                                                             @Validated({Default.class}) @RequestBody PatientResquestDTO patientResquestDTO){
         return ResponseEntity.ok(patientService.updatePatient(email,patientResquestDTO));
     }
 
     @DeleteMapping(path = "/{email}")
+    @Operation(summary = "Delete Patients")
     public ResponseEntity<?> deletePatient(@PathVariable String email){
         patientService.deletePatient(email);
         return ResponseEntity.ok("Deletion SuccessFull");
