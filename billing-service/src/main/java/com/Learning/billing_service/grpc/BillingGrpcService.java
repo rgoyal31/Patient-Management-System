1426d65d5295f9107f6145a9b15e5b9976534f25
prgoyal1 <rakshit.goyal@amadeus.com>
2025-05-04 00:42:19 +0530

    Adding the BillingService,code will be generated via the proto file on mvn compile, grpc server created

---
+++ billing-service/src/main/java/com/Learning/billing_service/grpc/BillingGrpcService.java
@@ -0,0 +1,31 @@
+package com.Learning.billing_service.grpc;
+
+import billing.BillingRequest;
+import billing.BillingResponse;
+import billing.BillingServiceGrpc.BillingServiceImplBase;
+import io.grpc.stub.StreamObserver;
+import lombok.extern.slf4j.Slf4j;
+import lombok.extern.slf4j.XSlf4j;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+@Slf4j
+public class BillingGrpcService extends BillingServiceImplBase{
+
+    @Override
+    public void CreateBillingAccount(BillingRequest billingRequest,
+                                     StreamObserver<billing.BillingResponse> responseObserver){
+
+        log.info("createBilling Account Request Received {}", billingRequest.toString());
+
+        //Business logic- eg. save to db, perform calculation etc
+
+        BillingResponse billingResponse = BillingResponse.newBuilder()
+                .setAccountId("123456")
+                .setStatus("ACTIVE")
+                .build();
+
+        responseObserver.onNext(billingResponse);
+        responseObserver.onCompleted();
+    }
+}
